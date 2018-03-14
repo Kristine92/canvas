b1bde9a9e21372ff6a93b273c8807403a714a4b0
Kristine Laranjo <kristine@Kristines-MacBook-Pro.local>
2018-03-14 14:25:16 -0700

    User can use a pan gesture to move the position of the tray

---
+++ Canvas/Canvas/Canvas/ViewControllers/CanvasViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CanvasViewController.swift
+//  Canvas
+//
+//  Created by Kristine Laranjo on 3/14/18.
+//  Copyright © 2018 Kristine Laranjo. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            print("Gesture began")
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            print("Gesture ended")
+            
+        }
+    }
+    
+}
